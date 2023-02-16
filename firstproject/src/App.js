4bffac5b3ad91f5c8efc0367df699831600d1f88
OussamaRguez <oussema.rguez@gmail.com>
2023-02-16 21:11:59 +0100

    first commit

---
+++ firstproject/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return ( 
+    <>
+    <h1>3id thawret Libya, Marhba Zied</h1>
+    <form>
+      <div class="mb-3">
+        <label  for="exampleFormControlInput1" class="form-label lab">Email address</label>
+        <input  type="email" class="form-control inp" id="exampleFormControlInput1" placeholder="name@example.com"/>
+      </div>
+      <div class="mb-3">
+        <label  for="exampleFormControlTextarea1" class="form-label lab">Example textarea</label>
+        <textarea class="form-control inp" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+
+    </form>
+    </>
+
+  )
+}
+
+export default App;
